ec3d5662363a6995ca3e40d6aecdc8c54532dc58
sepehr javid <javid.sepehr77@gmail.com>
2022-01-02 20:41:03 +0100

    add docker

--- scripts/cloud_server.sh
+++ scripts/cloud_server.sh
@@ -7,6 +7,16 @@ route add default gw 10.1.0.1
 iptables-save > /etc/iptables/rules.v4
 ip6tables-save > /etc/iptables/rules.v6
 
-## Install app
+
+##install docker
+cd /home/vagrant
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh ./get-docker.sh
+
+##build image
 cd /home/vagrant/server_app
-npm install
+docker build . -t cloud/node-web-app
+
+##run containers
+docker run -p 9999:8080 -d cloud/node-web-app
+docker run -p 9998:8080 -d cloud/node-web-app
