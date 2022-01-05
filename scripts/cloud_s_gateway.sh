d09f955314ae6cef4ac5f6b9ee107983cb8d062d
Matteo Calabrese <matteo.calabrese@aalto.fi>
2022-01-05 02:28:22 +0200

    Fixed comments

--- scripts/cloud_s_gateway.sh
+++ scripts/cloud_s_gateway.sh
@@ -10,11 +10,11 @@ iptables -t nat -A POSTROUTING -o enp0s8 -j MASQUERADE
 iptables -t nat -A PREROUTING -i enp0s8 -p tcp --source 172.18.18.18 --dport 8080 -j DNAT --to-destination 10.1.0.2:9999
 iptables -t nat -A PREROUTING -i enp0s8 -p tcp --source 172.16.16.16 --dport 8080 -j DNAT --to-destination 10.1.0.2:9998
 
-##Iptables rules
+## Iptables rules
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT  #Accept ike trrafic
 iptables -A INPUT -p esp -i enp0s8 --source 172.16.16.16 -j ACCEPT      #Acccept esp encapsulated trrafic from enp0s8
 iptables -A INPUT -p esp -i enp0s8 --source 172.18.18.18 -j ACCEPT      #Acccept esp encapsulated trrafic from enp0s8
-iptables -A INPUT -i enp0s3 -j ACCEPT                                   #Accept vagrant ssh trrafic from enp0s8
+iptables -A INPUT -i enp0s3 -j ACCEPT                                   #Accept vagrant ssh trrafic from enp0s3
 iptables -A INPUT -i enp0s9 --source 10.1.0.0/16 -j ACCEPT              #ACCEPT internal trrafic from enp0s9
 iptables -P INPUT DROP                                                  #DROP everything else policy
 
